de132fd500764aca0643ef409b4fe45509284673
João Sá <joao.vsaa90@gmail.com>
2024-05-13 11:51:43 -0300

    feat: adding a Test Component to verify tailwind

--- instagram-alike/src/main.tsx
+++ instagram-alike/src/main.tsx
@@ -1,10 +1,13 @@
+/** @format */
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
+import Test from './test.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+    <Test />
+  </React.StrictMode>
 )

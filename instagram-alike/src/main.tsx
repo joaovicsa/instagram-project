1542b4f32fb6b903dd16bf282a0f956ee771fbe8
João Sá <joao.vsaa90@gmail.com>
2024-05-13 11:52:26 -0300

    delete: removing test component

--- instagram-alike/src/main.tsx
+++ instagram-alike/src/main.tsx
@@ -3,11 +3,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import Test from './test.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
-    <Test />
   </React.StrictMode>
 )

d5994ba8fa853496a6ffa465eebfc8391695072b
João Sá <joao.vsaa90@gmail.com>
2024-05-15 18:03:04 -0300

    add: add input component to login template

--- instagram-alike/src/templates/login.tsx
+++ instagram-alike/src/templates/login.tsx
@@ -1,5 +1,7 @@
 /** @format */
 
+import { Input } from '../components/input'
+
 function Login() {
   return (
     <div className="container border-gray border-2 w-96 h-[400px] flex items-center flex-wrap">
@@ -8,26 +10,8 @@ function Login() {
       </div>
       <section className="container">
         <div className="container h-20 flex flex-wrap justify-center">
-          <div id="input" className="flex flex-wrap w-full">
-            <p className="w-full h-4 text-xs text-gray-400">
-              Telefone, nome de usuário ou email:
-            </p>
-            <input
-              className="w-full h-4 bg-stone-100"
-              type="text"
-              name="username"
-              id="username"
-            />
-          </div>
-          <div id="input" className="flex flex-wrap w-full">
-            <p className="w-full h-4 text-xs text-gray-400">Senha:</p>
-            <input
-              className="w-full h-4 bg-stone-100"
-              type="password"
-              name="user password"
-              id="userpassword"
-            />
-          </div>
+          <Input description="Telefone, nome de usuário ou email:" type="text" />
+          <Input description="Senha:" type="password" />
         </div>
         <div className="container flex flex-wrap justify-center">
           <button className="bg-blue-500 w-64 rounded-md h-8 text-white" type="submit">

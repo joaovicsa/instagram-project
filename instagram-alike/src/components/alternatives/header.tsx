7741cafc7e8d80a8f865b957bc3d952bffbdcf6a
João Sá <joao.vsaa90@gmail.com>
2024-05-15 20:51:42 -0300

    add: adding the Alternatives Logins header Component

---
+++ instagram-alike/src/components/alternatives/header.tsx
@@ -0,0 +1,13 @@
+/** @format */
+
+export const Header = (): JSX.Element => {
+  return (
+    <div>
+      <div className="container flex flex-wrap justify-evenly items-center gap-x-5">
+        <hr className="w-28 h-fit" />
+        <p className="text-center text-gray-500">OU</p>
+        <hr className="w-28 h-fit" />
+      </div>
+    </div>
+  )
+}

5a56dbd7ac3586c2eaba9c204fe872a8bf3cf44f
João Sá <joao.vsaa90@gmail.com>
2024-05-15 20:50:38 -0300

    add: add index componet of Alternatives logins

---
+++ instagram-alike/src/components/alternatives/root.tsx
@@ -0,0 +1,13 @@
+/** @format */
+
+import { GoogleButton } from './google'
+import { Header } from './header'
+
+export const AlternativeRoot = (): JSX.Element => {
+  return (
+    <>
+      <Header />
+      <GoogleButton />
+    </>
+  )
+}

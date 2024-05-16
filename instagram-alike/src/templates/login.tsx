8e59a5e6b7316d2538d95df845b3c74af3b2c724
João Sá <joao.vsaa90@gmail.com>
2024-05-16 19:28:04 -0300

    styled: setting up the landing page layout

--- instagram-alike/src/templates/login.tsx
+++ instagram-alike/src/templates/login.tsx
@@ -7,26 +7,35 @@ import { SignUp } from '../components/signup'
 
 function Login() {
   return (
-    <div className="w-96 flex flex-wrap gap-2">
-      <div className="border-gray border-2 w-96 h-[470px] flex items-center justify-center flex-wrap">
-        <div className="w-full h-28 flex justify-center items-center">
-          <h1 className="w-full text-5xl text-center">Postgram</h1>
-        </div>
-        <section className="container">
-          <div className="container h-20 flex flex-wrap justify-center">
-            <Input description="Telefone, nome de usuário ou email" type="text" />
-            <Input description="Senha" type="password" />
-          </div>
-          <div className="container flex flex-wrap justify-center">
-            <button className="bg-blue-500 w-64 rounded-md h-8 text-white" type="submit">
-              Entrar
-            </button>
-          </div>
-        </section>
-        <Alternatives />
+    <div id="layout" className="flex flex-wrap justify-center items-center">
+      <div id="upper-left">
+        
       </div>
-      <SignUp />
-      <GetApp />
+      <div id="upper-right">
+        <div className="w-96 flex flex-wrap gap-2">
+          <div className="border-gray border-2 w-96 h-[470px] flex items-center justify-center flex-wrap">
+            <div className="w-full h-28 flex justify-center items-center">
+              <h1 className="w-full text-5xl text-center">Postgram</h1>
+            </div>
+            <section className="container">
+              <div className="container h-20 flex flex-wrap justify-center">
+                <Input description="Telefone, nome de usuário ou email" type="text" />
+                <Input description="Senha" type="password" />
+              </div>
+              <div className="container flex flex-wrap justify-center">
+                <button
+                  className="bg-blue-500 w-64 rounded-md h-8 text-white"
+                  type="submit">
+                  Entrar
+                </button>
+              </div>
+            </section>
+            <Alternatives />
+          </div>
+          <SignUp />
+          <GetApp />
+        </div>
+      </div>
     </div>
   )
 }

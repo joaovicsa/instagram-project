151a3465c2a6574a65238b3d92f4fce12d40dd36
João Sá <joao.vsaa90@gmail.com>
2024-05-16 14:43:54 -0300

    feat: adds create account component

--- instagram-alike/src/templates/login.tsx
+++ instagram-alike/src/templates/login.tsx
@@ -2,25 +2,29 @@
 
 import { Alternatives } from '../components/alternatives'
 import { Input } from '../components/input'
+import { SignUp } from '../components/signup'
 
 function Login() {
   return (
-    <div className="container border-gray border-2 w-96 h-[400px] flex items-center justify-center flex-wrap">
-      <div className="w-full h-28">
-        <h1 className="w-full h-full text-5xl text-center">Postgram</h1>
+    <div className="w-96 flex flex-wrap gap-2">
+      <div className="border-gray border-2 w-96 h-[400px] flex items-center justify-center flex-wrap">
+        <div className="w-full h-28">
+          <h1 className="w-full h-full text-5xl text-center">Postgram</h1>
+        </div>
+        <section className="container">
+          <div className="container h-20 flex flex-wrap justify-center">
+            <Input description="Telefone, nome de usuário ou email" type="text" />
+            <Input description="Senha" type="password" />
+          </div>
+          <div className="container flex flex-wrap justify-center">
+            <button className="bg-blue-500 w-64 rounded-md h-8 text-white" type="submit">
+              Entrar
+            </button>
+          </div>
+        </section>
+        <Alternatives />
       </div>
-      <section className="container">
-        <div className="container h-20 flex flex-wrap justify-center">
-          <Input description="Telefone, nome de usuário ou email" type="text" />
-          <Input description="Senha" type="password" />
-        </div>
-        <div className="container flex flex-wrap justify-center">
-          <button className="bg-blue-500 w-64 rounded-md h-8 text-white" type="submit">
-            Entrar
-          </button>
-        </div>
-      </section>
-      <Alternatives />
+      <SignUp />
     </div>
   )
 }

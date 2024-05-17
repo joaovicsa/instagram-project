ba79c875a300bfbd5c0ae4c930aabd4d7a9e69f2
João Sá <joao.vsaa90@gmail.com>
2024-05-16 21:22:34 -0300

    refactor: separates the main layout from login component

--- instagram-alike/src/routes/postgram.tsx
+++ instagram-alike/src/routes/postgram.tsx
@@ -1,10 +1,16 @@
 /** @format */
 
-import Login from '../templates/login'
+import { SlideShow } from '../components/slideshow'
+import { Login } from '../templates/login'
 import '/src/index.css'
 
 function Postgram() {
-  return <Login />
+  return (
+    <div id="layout" className="flex flex-wrap justify-center items-center">
+      <SlideShow />
+      <Login />
+    </div>
+  )
 }
 
 export default Postgram
--- instagram-alike/src/templates/login.tsx
+++ instagram-alike/src/templates/login.tsx
@@ -5,39 +5,32 @@ import { GetApp } from '../components/getapp'
 import { Input } from '../components/input'
 import { SignUp } from '../components/signup'
 
-function Login() {
+export const Login = (): JSX.Element => {
   return (
-    <div id="layout" className="flex flex-wrap justify-center items-center">
-      <div id="upper-left">
-        
-      </div>
-      <div id="upper-right">
-        <div className="w-96 flex flex-wrap gap-2">
-          <div className="border-gray border-2 w-96 h-[470px] flex items-center justify-center flex-wrap">
-            <div className="w-full h-28 flex justify-center items-center">
-              <h1 className="w-full text-5xl text-center">Postgram</h1>
-            </div>
-            <section className="container">
-              <div className="container h-20 flex flex-wrap justify-center">
-                <Input description="Telefone, nome de usuário ou email" type="text" />
-                <Input description="Senha" type="password" />
-              </div>
-              <div className="container flex flex-wrap justify-center">
-                <button
-                  className="bg-blue-500 w-64 rounded-md h-8 text-white"
-                  type="submit">
-                  Entrar
-                </button>
-              </div>
-            </section>
-            <Alternatives />
+    <div id="upper-right">
+      <div className="w-96 flex flex-wrap gap-2">
+        <div className="border-gray border-2 w-96 h-[470px] flex items-center justify-center flex-wrap">
+          <div className="w-full h-28 flex justify-center items-center">
+            <h1 className="w-full text-5xl text-center">Postgram</h1>
           </div>
-          <SignUp />
-          <GetApp />
+          <section className="container">
+            <div className="container h-20 flex flex-wrap justify-center">
+              <Input description="Telefone, nome de usuário ou email" type="text" />
+              <Input description="Senha" type="password" />
+            </div>
+            <div className="container flex flex-wrap justify-center">
+              <button
+                className="bg-blue-500 w-64 rounded-md h-8 text-white"
+                type="submit">
+                Entrar
+              </button>
+            </div>
+          </section>
+          <Alternatives />
         </div>
+        <SignUp />
+        <GetApp />
       </div>
     </div>
   )
 }
-
-export default Login

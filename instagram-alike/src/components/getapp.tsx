0e8f070da9fbc214b504b82af8dd682ad193d539
João Sá <joao.vsaa90@gmail.com>
2024-05-28 14:44:18 -0300

    styled: adding responsiveness to login templates & components

--- instagram-alike/src/components/getapp.tsx
+++ instagram-alike/src/components/getapp.tsx
@@ -2,51 +2,57 @@
 
 export const GetApp = (): JSX.Element => {
   return (
-    <div className="w-96 grid grid-rows-2 grid-cols-2">
-      <div className="row-span-1 col-span-2 row-start-1 col-start-1 flex justify-center items-center">
+    <div className="md:w-96 md:grid md:grid-rows-2 md:grid-cols-2 md:h-fit w-full h-20 flex flex-wrap">
+      <div className="md:row-span-1 md:col-span-2 md:row-start-1 md:col-start-1 w-full flex justify-center items-center mb-4 md:mb-0">
         <span>Obtenha o aplicativo.</span>
       </div>
-      <div className="row-span-1 col-span-1 row-start-2 col-start-1 flex justify-center items-center">
-        <button>
-          <div
-            className="grid grid-rows-2 grid-cols-3 bg-black text-white w-44 h-16 rounded-lg
+      <div className="md:row-span-1 md:col-span-2 md:row-start-2 md:col-start-1 w-full flex justify-center items-center gap-2">
+        <div className="md:row-span-1 md:col-span-1 md:row-start-2 md:col-start-1 flex justify-center items-center">
+          <button>
+            <div
+              className="grid grid-rows-2 grid-cols-3 bg-black text-white md:w-44 w-36 h-14 rounded-lg
         ">
-            <div className="row-start-1 col-start-1 row-span-2 col-span-1 flex justify-center items-center h-full">
-              <img
-                className=" h-11"
-                src="../media/disponivelgoogle.png"
-                alt="disponível no google"
-              />
+              <div className="row-start-1 col-start-1 row-span-2 col-span-1 flex justify-center items-center h-full">
+                <img
+                  className="md:h-11 h-9"
+                  src="../media/disponivelgoogle.png"
+                  alt="disponível no google"
+                />
+              </div>
+              <div className="col-start-2 col-span-2 row-span-1 flex justify-start items-end">
+                <span className="text-xs uppercase ">disponível no</span>
+              </div>
+              <div className="col-start-2 col-span-2 row-span-1 flex justify-start items-start mb-2">
+                <span className="capitalize text-md md:text-lg lg:text-xl">
+                  google play
+                </span>
+              </div>
             </div>
-            <div className="col-start-2 col-span-2 row-span-1 flex justify-start items-end">
-              <span className="text-xs uppercase ">disponível no</span>
-            </div>
-            <div className="col-start-2 col-span-2 row-span-1 flex justify-start items-start mb-2">
-              <span className="capitalize text-xl">google play</span>
-            </div>
-          </div>
-        </button>
-      </div>
-      <div className="row-span-1 col-span-1 row-start-2 col-start-2 flex justify-center items-center">
-        <button>
-          <div
-            className="grid grid-rows-2 grid-cols-3 bg-black text-white w-44 h-16 rounded-lg
+          </button>
+        </div>
+        <div className="row-span-1 col-span-1 row-start-2 col-start-2 flex justify-center items-center">
+          <button>
+            <div
+              className="grid grid-rows-2 grid-cols-3 bg-black text-white md:w-44 w-36 h-14 rounded-lg
         ">
-            <div className="row-start-1 col-start-1 row-span-2 col-span-1 flex justify-center items-center h-full">
-              <img
-                className=" h-11"
-                src="../media/disponivelmicrosoft.png"
-                alt="baixe da microsoft"
-              />
+              <div className="row-start-1 col-start-1 row-span-2 col-span-1 flex justify-center items-center h-full">
+                <img
+                  className="md:h-11 h-9"
+                  src="../media/disponivelmicrosoft.png"
+                  alt="baixe da microsoft"
+                />
+              </div>
+              <div className="col-start-2 col-span-2 row-span-1 flex justify-start items-end">
+                <span className="text-xs uppercase ">baixe da</span>
+              </div>
+              <div className="col-start-2 col-span-2 row-span-1 flex justify-start items-start mb-2">
+                <span className="capitalize text-md md:text-lg lg:text-xl">
+                  microsoft
+                </span>
+              </div>
             </div>
-            <div className="col-start-2 col-span-2 row-span-1 flex justify-start items-end">
-              <span className="text-xs uppercase ">baixe da</span>
-            </div>
-            <div className="col-start-2 col-span-2 row-span-1 flex justify-start items-start mb-2">
-              <span className="capitalize text-xl">microsoft</span>
-            </div>
-          </div>
-        </button>
+          </button>
+        </div>
       </div>
     </div>
   )
--- instagram-alike/src/components/signup.tsx
+++ instagram-alike/src/components/signup.tsx
@@ -2,7 +2,7 @@
 
 export const SignUp = (): JSX.Element => {
   return (
-    <div className="border-gray border-2 w-96 h-16 flex justify-center items-center">
+    <div className="md:border-gray md:border-2 w-96 h-32 md:h-16 flex justify-center items-center">
       <span className="text-black">
         Não tem uma conta?{' '}
         <a href="./" className="text-blue-700">
